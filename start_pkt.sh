9e92db52829ff6aac92fbcdf403ba98470f66f0a
corpltd001 <corpltd001@outlook.com>
2024-12-30 13:59:16 +0530

    Create start_pkt.sh

---
+++ start_pkt.sh
@@ -0,0 +1,1 @@
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt && chmod +x packetcrypt && ./packetcrypt ann -p pkt1q4krnrt8rquv4qhzr9cuvlevmaewvep70z00k00 http://pool.pkt.world
